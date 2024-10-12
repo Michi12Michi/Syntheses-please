302dac994fa826b5970a03f87c981fa46962e00b
Michi12Michi <michele.loriso@studenti.unibas.it>
2024-10-12 11:19:52 +0200

    Update game.js

--- new_app/js/game.js
+++ new_app/js/game.js
@@ -2,20 +2,27 @@
 
 const lavoro = document.body.querySelector("div#lavoro");
 const materiali = document.body.querySelector("div#materiali");
+const categorie = document.body.querySelector("div#categorie");
 
-lavoro.addEventListener('dragover', (e) => {
-    e.preventDefault();
+[materiali, lavoro, categorie].forEach((element) => {
+    element.addEventListener("dragover", (e) => {
+        e.preventDefault();
+    })
 });
 
-materiali.addEventListener("dragover", (e) => {
-    e.preventDefault();
+[materiali, categorie].forEach((element) => {
+    element.addEventListener("drop", (e) => {
+        e.preventDefault();
+        console.log(e);
+    })
 });
 
 lavoro.addEventListener('drop', (e) => {
     e.preventDefault();
     const droppedElementNome = e.dataTransfer.getData('nome');
     const droppedElement = materiali.querySelector(`[data-nome='${droppedElementNome}']`)
-    if (droppedElement) {
+    const alreadyPresentElement = lavoro.querySelector(`[data-nome='${droppedElementNome}']`);
+    if (droppedElement && ! alreadyPresentElement) {
         const newElement = new Materiale(droppedElementNome, lavoro, false);
         newElement.HTMLelement.style.position = 'absolute';
         newElement.HTMLelement.style.left = `${e.offsetX - 50}px`;
