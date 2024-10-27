f2441cc234b9d2d68847f55300db3f9aba05fbea
Michi12Michi <michele.loriso@studenti.unibas.it>
2024-10-27 23:26:36 +0100

    riscrittura parziale checkReactor

--- new_app/js/main.js
+++ new_app/js/main.js
@@ -44,7 +44,7 @@ class GameObject {
         // tra le quest accettate, chiudi positivamente quelle le cui condizioni sono raggiunte; le condizioni per chiuderle negativamente sono direttamente conseguenti attività dell'utente, come il passaggio di livello e il rifiuto. Verranno trattate separatamente
         this.quest_active_list.forEach(element => {
             // TODO: per ogni quest accettata controlla le condizioni di riuscita sul DB
-            // query = `SELECT * FROM quests WHERE id = element;`
+            // const query = `SELECT * FROM quests WHERE id = element;`
             // if (this.material_discovered_list.includes(query.objective_material)) {
                 // TODO: gestisci le conseguenze delle quest terminate positivamente (aggiorna $, materiali, exp)
                 // this.credit += ;
@@ -173,63 +173,77 @@ class GameObject {
 
     checkReactor() {
         // Controlla se gli elementi, senza ordine, creano un nuovo composto
-        const query_material_ids = this.material_to_combine_list.join(", ");
-        const query_material_length = String(this.material_to_combine_list.length);
+        // creo placeholder
+        const material_query = new Array(this.material_to_combine_list.length).fill("?").join(", ");
         // ottengo id combinazione e id materiale prodotto
-        var query_existing_combination = `SELECT cm1.combination_id, cm2.material_id AS material_id_with_product_1
-        FROM combination_material cm1
-        JOIN combination_material cm2 ON cm1.combination_id = cm2.combination_id
-        WHERE cm1.product = 0 AND cm1.material_id IN ${query_material_ids}
-        AND cm2.product = 1
-        /GROUP BY cm1.combination_id
-        HAVING COUNT(DISTINCT cm1.material_id) = ${query_material_length};`
-        if (query_existing_combination) {
-            // recupero blog_id e level_id dalla combinazione
-            let query_combination_properties = `SELECT blog_id, level_id FROM combinations c WHERE c.id = ...;` // TODO: INSERIRE ID COMBINAZIONE
-            // il controllo vero e proprio sugli esiti della combinazione parte dal valutare il livello attuale
-            if (0 <= this.level) { // TODO: INSERIRE IL LIVELLO DERIVANTE DALLA QUERY
-                // recupero tutte le proprietà del prodotto
-                let query_product_properties = `SELECT * FROM materials WHERE id = ...;`; // TODO: inserire id materiale prodotto
-                let id_materiale = 1, price_materiale = 12, experience_materiale = 50, id_combinazione = 999;
-                // verifica se ho già fatto questa reazione
-                // non ho bisogno di aggiornare la lista materiali nè l'esperienza, ma solo i soldi e la mappa reazioni
-                // non ho bisogno di triggerare altro
-                if (combination_done_list.has(id_combinazione)) {
-                    // sarà stata fatta un certo numero di volte, per cui aggiorno i soldi e la Map
-                    let times_reaction = this.combination_done_list.get(id_combinazione);
-                    // guadagno originale(4-volte)/(2+volte^2) ----> come suggeriva alessandro 
-                    this.credit = this.credit + price_materiale*((4 - times_reaction)/(2 + Math.pow(times_reaction, 2)));
-                    this.combination_done_list.set(id_combinazione, times_reaction + 1);
+        const query_existing_combination = `SELECT cm2.material_id, cm2.combination_id
+                                            FROM combination_material cm2
+                                            WHERE cm2.product = 1
+                                            AND cm2.combination_id IN (
+                                                SELECT cm1.combination_id
+                                                FROM combination_material cm1
+                                                WHERE cm1.product = 0
+                                                AND cm1.material_id IN (${material_query})
+                                                GROUP BY cm1.combination_id
+                                                HAVING COUNT(*) = ?);`;
+        
+        window.sqlitePlugin.openDatabase({ name: "chimgio.db", location: "default" }, (db) => {
+            db.executeSql(query_existing_combination, [this.material_to_combine_list, this.material_to_combine_list.length], (res) => {
+                // megablocco risultato positivo con eventuali più elementi
+                if (res.rows.length > 0) {
+                    const successful_combination_id = res.rows.item(0).combination_id;
+                    // recupero blog_id e level_id dalla combinazione
+                    const query_combination_properties = `SELECT blog_id, level_id FROM combinations c WHERE c.id = ?;`
+                    db.executeSql(query_combination_properties, [successful_combination_id], (matRes) => {
+                        const at_which_level = matRes.rows.item(0).level_id;
+                        const which_blog_id = matRes.rows.item(0).blog_id;
+                        // il controllo vero e proprio sugli esiti della combinazione parte dal valutare il livello attuale
+                        if (at_which_level <= this.level) {
+                            
+                            
+                            let query_product_properties = `SELECT * FROM materials WHERE id = ...;`; // TODO: inserire id materiale prodotto
+                            let id_materiale = 1, price_materiale = 12, experience_materiale = 50, id_combinazione = 999;
+                            // verifica se ho già fatto questa reazione
+                            // non ho bisogno di aggiornare la lista materiali nè l'esperienza, ma solo i soldi e la mappa reazioni
+                            // non ho bisogno di triggerare altro
+                            if (combination_done_list.has(id_combinazione)) {
+                                // sarà stata fatta un certo numero di volte, per cui aggiorno i soldi e la Map
+                                let times_reaction = this.combination_done_list.get(id_combinazione);
+                                // guadagno originale(4-volte)/(2+volte^2) ----> come suggeriva alessandro 
+                                this.credit = this.credit + price_materiale*((4 - times_reaction)/(2 + Math.pow(times_reaction, 2)));
+                                this.combination_done_list.set(id_combinazione, times_reaction + 1);
+                            } else {
+                                // se non ho mai fatto la reazione aggiorno tutti i parametri*
+                                // a) aggiorno lista reazioni fatte
+                                this.combination_done_list.set(id_combinazione, 1);
+                                // b) aggiorno soldi ed esperienza
+                                this.credit = this.credit + price_materiale;
+                                this.experience = this.experience + experience_materiale;
+                                // c) verificare se il composto non ce l'ho nella lista
+                                // non dovrei preoccuparmi del fatto che ce l'ho, perché ce l'ho già salvato
+                                if (!(this.material_discovered_list.has(id_materiale))) {
+                                    // QUI ENTRO SOLO SE IL COMPOSTO è STATO FATTO PER LA PRIMA VOLTA 
+                                    this.material_discovered_list.push(id_materiale);
+                                    // dovrebbe partire una animazione standard di scopertauaochefigo e devo passare immagine e descrizione del materiale
+                                    // TODO: funzione discoverMaterial() -> si potrebbero passare tutte le proprietà, oppure solo l'id per recuperare con 
+                                    // una query tutto il necessario*
+                                    // e dovrei anche verificare se appartiene ad una categoria che già ho sbloccato, no?
+                                    // faccio una query su material_category?
+                                    // in caso la categoria ce l'ho, sti cazzi, altrimenti devo chiamare Categoria(id_cat) per il rendering
+                                }
+                            }
+                        }
+                    });
+                    
+                    // finalmente svuoto il reattore -> la combinazione è valida, anche in caso in cui non ci sia il livello.
+                    this.empty();
+                    afterPlayerInteraction(); // controlla a cosa porteranno questi cambiamenti nello stato del gioco
                 } else {
-                    // se non ho mai fatto la reazione aggiorno tutti i parametri*
-                    // a) aggiorno lista reazioni fatte
-                    this.combination_done_list.set(id_combinazione, 1);
-                    // b) aggiorno soldi ed esperienza
-                    this.credit = this.credit + price_materiale;
-                    this.experience = this.experience + experience_materiale;
-                    // c) verificare se il composto non ce l'ho nella lista
-                    // non dovrei preoccuparmi del fatto che ce l'ho, perché ce l'ho già salvato
-                    if (!(this.material_discovered_list.has(id_materiale))) {
-                        // QUI ENTRO SOLO SE IL COMPOSTO è STATO FATTO PER LA PRIMA VOLTA 
-                        this.material_discovered_list.push(id_materiale);
-                        // dovrebbe partire una animazione standard di scopertauaochefigo e devo passare immagine e descrizione del materiale
-                        // TODO: funzione discoverMaterial() -> si potrebbero passare tutte le proprietà, oppure solo l'id per recuperare con 
-                        // una query tutto il necessario*
-                        // e dovrei anche verificare se appartiene ad una categoria che già ho sbloccato, no?
-                        // faccio una query su material_category?
-                        // in caso la categoria ce l'ho, sti cazzi, altrimenti devo chiamare Categoria(id_cat) per il rendering
-                    }
+                    // la combinazione non ha portato a frutti
+                    if (this.material_to_combine_list.length == max_items_per_combination)
+                        this.empty();
                 }
-            } else {
-                console.log("Non hai livello coglioncello");
-            }
-            // finalmente svuoto il reattore -> la combinazione è valida, anche in caso in cui non ci sia il livello.
-            this.empty();
-            afterPlayerInteraction(); // controlla a cosa porteranno questi cambiamenti nello stato del gioco
-        } else {
-            // la combinazione non ha portato a frutti
-            if (this.material_to_combine_list.length == max_items_per_combination)
-                this.empty();
-        }
+            });
+        });
     }
 }
