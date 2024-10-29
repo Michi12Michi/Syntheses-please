f85306b981ea3648f42b21628c6dc33e1c0eb755
Michi12Michi <michele.loriso@studenti.unibas.it>
2024-10-29 23:01:00 +0100

    layout e logica

--- navigazione_e_varie/gioco/js/main.js
+++ navigazione_e_varie/gioco/js/main.js
@@ -1,1 +1,45 @@
-// qui va dichiarata tutta la roba globale, come il GO e funzioni di popolamento e renderizzazione
+document.addEventListener("init", function(event) {
+    // recupero un fun fact dal db e renderizzo
+    if (event.target.id === "loading") {
+        // window.sqlitePlugin.openDatabase({name: "chimgio.db", location: "default" }, (db) => {
+        //     const fun_fact_query = `SELECT * FROM fun_facts ORDER BY RANDOM() LIMIT 1;`
+        //     db.executeSql(fun_fact_query, [], (res) => {
+        //         if (res.rows.length > 0) {
+        //             document.querySelector("#funfact").innerHTML(res);
+        //         }
+        //     });
+        // });
+        // TODO: si dovrebbe istanziare il game object
+        // simulo un grossolano caricamento
+        setTimeout(() => {
+            // a resetPage va allegato un segnale (dati esistenti/non esistenti) affinché siano renderizzati i giusti buttons
+            document.querySelector("#navigator").resetToPage("menu.html", {data: {continue: true}});
+        }, 10000); 
+    } else if (event.target.id === "menu") {
+        // recupero il segnale
+        const signal = event.target.data.continue;
+        if (signal == true) {
+            // crea ed appende un pulsante "riprendi gioco" con la sua logica
+            var continue_btn = document.createElement("ons-button");
+            var div_to_append = document.querySelector("#menu .menu-div");
+            continue_btn.setAttribute("modifier", "cta");
+            continue_btn.textContent = "Riprendi";
+            // TODO: appendere event listener per chiamare al click la ripresa del gioco
+            div_to_append.insertBefore(continue_btn, div_to_append.firstChild);
+            // TODO: appendere pulsante per resettare i dati salvati e iniziare da capo
+            // la logica del pulsante nuovo gioco deve cambiare
+        }
+    }
+});
+
+function initGame() {
+    document.querySelector("#navigator").pushPage("main.html");
+}
+
+function readCredits() {
+    document.querySelector("#navigator").pushPage("credits.html");
+}
+
+function backToMenu() {
+    document.querySelector("#navigator").popPage("menu.html");
+}
